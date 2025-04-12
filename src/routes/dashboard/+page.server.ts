221a0e31dca1e35005ee8854099c6e4ffd2e2836
3b00d09 <bd315977@gmail.com>
2025-04-12 18:57:38 +0100

    stuff i forgot

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,6 +1,10 @@
 import type { PageServerLoad } from "./$types";
 
-export const load:PageServerLoad = async ({ locals }) => {
+export const load:PageServerLoad = async ({ locals, fetch }) => {
+
+    const req = await fetch("/api/owned-org")
+    const res = await req.json()
+    console.log(res)
     return {
         user:locals.user
     }
